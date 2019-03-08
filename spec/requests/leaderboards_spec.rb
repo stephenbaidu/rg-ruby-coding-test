89a13ab7ae438d66eda1ca6460bd3b22f3aba6a2
Stephen Baidu <stephenbaidu@gmail.com>
2019-03-08 20:21:13 +0000

    Improve request spec for /leaderboards

--- spec/requests/leaderboards_spec.rb
+++ spec/requests/leaderboards_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Leaderboards", type: :request do
   describe "GET /leaderboards" do
-    it "works! (now write some real specs)" do
+    let!(:leaderboard_1) { create :leaderboard }
+    let!(:leaderboard_2) { create :leaderboard }
+
+    before do
       get leaderboards_path
+    end
+
+    it "works! (now write some real specs)" do
       expect(response).to have_http_status(200)
+    end
+
+    it 'lists leaderboards' do
+      expect(response.body).to include(leaderboard_1.name)
+      expect(response.body).to include(leaderboard_2.name)
     end
   end
 end
